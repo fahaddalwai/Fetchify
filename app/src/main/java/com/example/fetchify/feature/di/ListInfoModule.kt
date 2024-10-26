b4c5b176003687e75fc5ca2c22e0b93d698a59d6
dalwa <dalwaifahad@gmail.com>
2024-10-25 21:57:02 -0500

    feat: Dagger Hilt

---
+++ app/src/main/java/com/example/fetchify/feature/di/ListInfoModule.kt
@@ -0,0 +1,58 @@
+package com.example.fetchify.feature.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.dictionaryapp.feature.data.local.ListInfoDatabase
+import com.example.fetchify.feature.data.local.Converters
+import com.example.fetchify.feature.data.remote.ListInfoApi
+import com.example.fetchify.feature.data.remote.ListInfoApi.Companion.BASE_URL
+import com.example.fetchify.feature.data.repository.ListInfoRepositoryImpl
+import com.example.fetchify.feature.data.util.GsonParser
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ListInfoModule {
+
+    @Provides
+    @Singleton
+    fun provideGetListInfoUseCase(repository: ListInfoRepository): GetListInfo {
+        return GetListInfo(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideListInfoRepository(
+        db: ListInfoDatabase,
+        api: ListInfoApi
+    ): ListInfoRepository {
+        return ListInfoRepositoryImpl(api, db.dao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideListInfoDatabase(app: Application): ListInfoDatabase {
+        return Room.databaseBuilder(
+            app, ListInfoDatabase::class.java, "listDatabase"
+        ).addTypeConverter(Converters(GsonParser(Gson())))
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFetchifyApi(): ListInfoApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ListInfoApi::class.java)
+    }
+}
