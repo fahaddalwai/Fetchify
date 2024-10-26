7686b7c08ed1469f54a06f000dbfc9afc025676e
dalwa <dalwaifahad@gmail.com>
2024-10-26 14:54:28 -0500

    fix: DB return sanitized

--- app/src/main/java/com/example/fetchify/feature/data/local/ListInfoDao.kt
+++ app/src/main/java/com/example/fetchify/feature/data/local/ListInfoDao.kt
@@ -16,7 +16,11 @@ interface ListInfoDao {
     @Query("DELETE FROM listinfoentity")
     suspend fun deleteListInfos()
 
-    @Query("SELECT * FROM listinfoentity")
+    @Query("""
+    SELECT * FROM listinfoentity
+    WHERE name IS NOT NULL AND name != ''
+    ORDER BY listId, name
+""")
     suspend fun getListInfos(): List<ListInfoEntity>
 
 }
